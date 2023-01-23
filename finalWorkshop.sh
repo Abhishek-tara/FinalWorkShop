9c14372b82d3fb0ee3b74083cb1e042abcf982bd
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-01-23 08:59:04 +0530

    Add: Code for Loop test added

---
+++ finalWorkshop.sh
@@ -0,0 +1,15 @@
+for (( i=1;i<=10;i++ ))
+do
+	echo $i 
+done
+
+printf '\n'
+
+for (( i=1;i<=10;i++ ))
+do
+	if (( $i != 4 && $i != 6 && $i!=8 ))
+        then
+                printf $i
+        fi
+
+done
